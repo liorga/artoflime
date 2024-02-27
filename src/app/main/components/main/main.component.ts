c84519966c05e115423503bac037a4b9c8ec5963
liorga <liorgal31892@gmail.com>
2024-02-27 18:30:27 +0200

    improved sidenav expend and collapse also for mobile view

--- src/app/main/components/main/main.component.ts
+++ src/app/main/components/main/main.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  isExpanded = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape])
+      .subscribe((result) => {
+        if (result.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
   }
+  ngOnInit(): void {}
 
+  toggleSidenav() {
+    this.isExpanded = !this.isExpanded;
+  }
+  closeSidenav() {
+    if (this.sidenav.mode === 'over') {
+      this.sidenav.close();
+    }
+  }
 }
