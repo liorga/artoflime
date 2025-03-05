7598a6697fae4eba3d855ceb17917247325d2182
liorga <liorgal31892@gmail.com>
2025-03-05 10:48:14 +0900

    added a slider component base to use the gliderjs lib working now with an hard coded array of slides next is to be images

--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { GalleryRoutingModule } from './gallery-routing.module';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { ArtoflimematerialModule } from '../artoflimematerial/artoflimematerial.module';
+import { SliderComponent } from '../core/slider/slider.component';
 
 @NgModule({
-  declarations: [GalleryComponent],
+  declarations: [GalleryComponent, SliderComponent],
   imports: [CommonModule, GalleryRoutingModule, ArtoflimematerialModule],
 })
 export class GalleryModule {}
---
+++ src/types/glider.d.ts
@@ -0,0 +1,9 @@
+declare module '@glidejs/glide/dist/glide.esm' {
+  const Glide: any;
+  export default Glide;
+}
+
+declare module '@glidejs/glide' {
+  const Glide: any;
+  export default Glide;
+}
